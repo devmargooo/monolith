1fbf98a25dcd26e43ae3ca5f05253b6210b0dd78
devmargooo <devmargooo@gmail.com>
2025-08-21 17:35:55 +0300

    feat(conf): add base config

--- apps/xdonate/src/app/app.module.ts
+++ apps/xdonate/src/app/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { FeatureApiModule } from './feature/feature-api.module';
+import { ConfigModule } from '@nestjs/config';
+import { BaseConfig } from '../base.config';
+import { ExtsystemModule } from './extsystem/extsystem.module';
 
 @Module({
-  imports: [UsersModule, FeatureApiModule],
+  imports: [
+    UsersModule,
+    FeatureApiModule,
+    ConfigModule.forRoot({
+      envFilePath: '../../.env',
+      isGlobal: true,
+      load: [BaseConfig],
+    }),
+    ExtsystemModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
